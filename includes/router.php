a155e41a1f2a60945c700ded841c385f119724d3
smoaking90 <cwsmoak90@gmail.com>
2025-02-08 14:12:24 +0100

    obfuscating

--- includes/router.php
+++ includes/router.php
@@ -8,6 +8,7 @@ const INDEX_ROUTE = 'index';
 
 
 function normalizeUri(string $uri): string {
+    $uri = strtok($uri, '?');
     $uri = strtolower(trim($uri, '/'));
     return $uri === INDEX_URI ? INDEX_ROUTE : $uri;
 }
--- templates/guestbook_get.php
+++ templates/guestbook_get.php
@@ -1,6 +1,21 @@
 <?php
+
+function obfuscateName(string $name): string{
+    if(strlen($name <= 2)){
+        return $name;
+    }
+
+    return substr($name, 0, 1) . str_repeat('*', strlen($name) - 2) . substr($name, -1);
+}
+
+function obfuscateEmail(string $email): string{
+    $parts = explode('@', $email);
+    $username = $parts[0];
+    $domain = $parts[1] ?? '';
+    $username = substr($username, 0 , 2) . str_repeat('*', max(0, strlen($username) - 2));
+    return "$username@$domain";
+}
 $messages = $data['messages'];
-// echo $undefinedVariable;
 ?>
 
 <section>
@@ -11,8 +26,8 @@ $messages = $data['messages'];
     
     <?php else: ?>
         <?php foreach($messages as $message): ?>
-            <h3><?=htmlspecialchars($message['name'])?></h3>
-            <p>Email: <?=htmlspecialchars($message['email'])?></p>
+            <h3><?=obfuscateName(htmlspecialchars($message['name']))?></h3>
+            <p>Email: <?=obfuscateEmail(htmlspecialchars($message['email']))?></p>
             <p>Message: <?=nl2br(htmlspecialchars($message['message']))?></p>
             <small>Posted on: <?=htmlspecialchars($message['created_at'])?></small>
         <?php endforeach ?>
